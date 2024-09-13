953e5d7ceb6ba25e7d4edb49e702ea1fa6f282f1
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2024-09-13 13:50:50 -0400

    Test Variations
    Logging for a copy,  will be removed

--- src/test/java/org/skriptlang/skript/test/tests/syntaxes/events/EvtBlockDropTest.java
+++ src/test/java/org/skriptlang/skript/test/tests/syntaxes/events/EvtBlockDropTest.java
@@ -1,37 +1,67 @@
 package org.skriptlang.skript.test.tests.syntaxes.events;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.bukkitutil.WorldUtils;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.TriggerItem;
+import ch.njol.skript.lang.util.ContextlessEvent;
 import ch.njol.skript.test.runner.SkriptJUnitTest;
+import ch.njol.skript.variables.Variables;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.easymock.EasyMock;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
 
 public class EvtBlockDropTest extends SkriptJUnitTest {
 
 	private Block oak;
-	private final Player easyPlayer = EasyMock.createMock(Player.class);
+	private Player easyPlayer;
+	private Effect breakEffect;
 
 	@Before
 	public void setBlocks() {
+		easyPlayer = EasyMock.niceMock(Player.class);
 		oak = setBlock(Material.OAK_LOG);
 		oak.setType(Material.OAK_LOG);
+		breakEffect = Effect.parse("make {_player} break (block at location(10.5, -58.5, 0.5)) ", null);
 	}
 
 	@Test
 	public void breakBlock() {
+		if (breakEffect == null)
+			Assert.fail("Break effect is null");
+
+		//ContextlessEvent event = ContextlessEvent.get();
+		//Variables.setVariable("player", getMockPlayer(), event, true);
+
 		Skript.adminBroadcast("I will now break you!");
-		//easyPlayer.getEquipment().setItemInMainHand(new ItemStack(Material.NETHERITE_AXE));
-		easyPlayer.breakBlock(oak);
+		easyPlayer.breakBlock(getBlock());
+		//easyPlayer.breakBlock(oak);
+		//EasyMock.expectLastCall();
+		//EasyMock.replay(easyPlayer);
+		//TriggerItem.walk(breakEffect, event);
+		//EasyMock.verify(easyPlayer);
 	}
 
 	@After
 	public void cleanBlock() {}
+
+	private Player getMockPlayer() {
+		InvocationHandler handler = (proxy, method, args) -> {
+			if (method.getName().equals("getFoodLevel"))
+				return 0;
+			return method.invoke(easyPlayer, args);
+		};
+		return (Player) Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] { Player.class }, handler);
+	}
 
 }
