cf9b0f4db94577b75c253bac1dfb4ea2d7a879cc
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2024-10-04 18:43:10 -0400

    Fix Test for older versions

--- src/test/java/org/skriptlang/skript/test/tests/syntaxes/events/EvtFireworkTest.java
+++ src/test/java/org/skriptlang/skript/test/tests/syntaxes/events/EvtFireworkTest.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Firework;
 import org.bukkit.event.Event;
 import org.bukkit.event.entity.FireworkExplodeEvent;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -20,11 +21,21 @@ import java.util.List;
 
 public class EvtFireworkTest extends SkriptJUnitTest {
 
+	private EntityType entityType;
+	@Before
+	public void getEntity() {
+		if (Skript.isRunningMinecraft(1, 20, 5)){
+			entityType = EntityType.FIREWORK_ROCKET;
+		} else {
+			entityType = EntityType.valueOf("FIREWORK");
+		}
+	}
+
 	@Test
 	public void callEvents() {
 		List<Event> events = new ArrayList<>();
 		for (SkriptColor color : SkriptColor.values()) {
-			Firework firework = (Firework) getTestWorld().spawnEntity(getTestLocation(), EntityType.FIREWORK_ROCKET);
+			Firework firework = (Firework) getTestWorld().spawnEntity(getTestLocation(), entityType);
 			FireworkEffect fireworkEffect = FireworkEffect.builder().withColor(color.asDyeColor().getFireworkColor()).build();
 			FireworkMeta fireworkMeta = firework.getFireworkMeta();
 			fireworkMeta.addEffects(fireworkEffect);
