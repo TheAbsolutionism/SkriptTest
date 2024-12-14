dc21aa629394133ecbed24b7d677ec07c457cff5
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2024-12-14 15:01:30 -0500

    Additional Patterns

--- src/main/java/ch/njol/skript/events/EvtVehicleCollision.java
+++ src/main/java/ch/njol/skript/events/EvtVehicleCollision.java
@@ -18,17 +18,23 @@ public class EvtVehicleCollision extends SkriptEvent {
 
 	static {
 		Skript.registerEvent("Vehicle Collision", EvtVehicleCollision.class, new Class[]{VehicleBlockCollisionEvent.class, VehicleEntityCollisionEvent.class}
-			,"vehicle collision [(with|of) [a[n]] %-itemtypes/blockdatas/entitydatas%]")
+			,"vehicle collision [(with|of) [a[n]] %-itemtypes/blockdatas/entitydatas%]",
+			"vehicle block collision [(with|of) [a[n]] %-itemtypes/blockdatas%]",
+			"vehicle entity collision [(with|of) [a[n]] %-entitydatas%]")
 				.description("Called when a vehicle collides with a block or entity.")
 				.examples("on vehicle collision:", "on vehicle collision with obsidian:", "on vehicle collision with a zombie:")
 				.since("INSERT VERSION");
 	}
 
 	private Literal<?> expr;
+	private boolean blockCollision;
+	private boolean entityCollision;
 
 	@Override
 	public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
 		expr = args[0];
+		blockCollision = matchedPattern == 1;
+		entityCollision = matchedPattern == 2;
 		return true;
 	}
 
@@ -37,8 +43,14 @@ public class EvtVehicleCollision extends SkriptEvent {
 		if (!(event instanceof VehicleCollisionEvent collisionEvent))
 			return false;
 
-		if (expr == null)
+		if (expr == null) {
+			if (blockCollision && !(event instanceof VehicleBlockCollisionEvent)) {
+				return false;
+			} else if (entityCollision && !(event instanceof VehicleEntityCollisionEvent)) {
+				return false;
+			}
 			return true;
+		}
 
 		ItemType item = null;
 		BlockData blockData = null;
