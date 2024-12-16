53c263456c0c4099bfce46f3487158daf0118681
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2024-12-15 20:34:18 -0500

    Proper Eval

--- src/main/java/ch/njol/skript/events/EvtVehicleCollision.java
+++ src/main/java/ch/njol/skript/events/EvtVehicleCollision.java
@@ -14,11 +14,13 @@ import org.bukkit.event.vehicle.VehicleCollisionEvent;
 import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 public class EvtVehicleCollision extends SkriptEvent {
 
 	static {
-		Skript.registerEvent("Vehicle Collision", EvtVehicleCollision.class, new Class[]{VehicleBlockCollisionEvent.class, VehicleEntityCollisionEvent.class}
-			,"vehicle collision [(with|of) [a[n]] %-itemtypes/blockdatas/entitydatas%]",
+		Skript.registerEvent("Vehicle Collision", EvtVehicleCollision.class, new Class[]{VehicleBlockCollisionEvent.class, VehicleEntityCollisionEvent.class},
+				"vehicle collision [(with|of) [a[n]] %-itemtypes/blockdatas/entitydatas%]",
 			"vehicle block collision [(with|of) [a[n]] %-itemtypes/blockdatas%]",
 			"vehicle entity collision [(with|of) [a[n]] %-entitydatas%]")
 				.description("Called when a vehicle collides with a block or entity.")
@@ -29,15 +31,23 @@ public class EvtVehicleCollision extends SkriptEvent {
 	private Literal<?> expr;
 	private boolean blockCollision;
 	private boolean entityCollision;
-	private boolean blockSpecific;
-	private Object[] types = null;
+	private List<ItemType> itemTypes;
+	private List<BlockData> blockDatas;
+	private List<EntityData<?>> entityDatas;
 
 	@Override
 	public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
 		if (args[0] != null) {
 			expr = args[0];
-			types = args[0].getArray();
-			blockSpecific = !(types instanceof EntityData<?>[]);
+			for (Object object : expr.getArray()) {
+				if (object instanceof ItemType itemType) {
+					itemTypes.add(itemType);
+				} else if (object instanceof BlockData blockData) {
+					blockDatas.add(blockData);
+				} else if (object instanceof EntityData<?> entityData) {
+					entityDatas.add(entityData);
+				}
+			}
 		}
 		blockCollision = matchedPattern == 1;
 		entityCollision = matchedPattern == 2;
@@ -49,7 +59,7 @@ public class EvtVehicleCollision extends SkriptEvent {
 		if (!(event instanceof VehicleCollisionEvent collisionEvent))
 			return false;
 
-		if (types == null) {
+		if (expr == null) {
 			if (blockCollision && !(event instanceof VehicleBlockCollisionEvent)) {
 				return false;
 			} else if (entityCollision && !(event instanceof VehicleEntityCollisionEvent)) {
@@ -58,21 +68,22 @@ public class EvtVehicleCollision extends SkriptEvent {
 			return true;
 		}
 
-		if (collisionEvent instanceof VehicleBlockCollisionEvent blockCollisionEvent && blockSpecific) {
-			Block block = blockCollisionEvent.getBlock();
-			ItemType itemType = new ItemType(block.getType());
-			BlockData blockData = block.getBlockData();
-			for (Object object : types) {
-				if (object instanceof ItemType itemType1 && itemType1.isSupertypeOf(itemType)) {
+		if (collisionEvent instanceof VehicleBlockCollisionEvent blockCollisionEvent) {
+			Block eventBlock = blockCollisionEvent.getBlock();
+			ItemType eventItemType = new ItemType(eventBlock.getType());
+			BlockData eventBlockData = eventBlock.getBlockData();
+			for (ItemType itemType : itemTypes) {
+				if (itemType.isSupertypeOf(eventItemType))
 					return true;
-				} else if (object instanceof BlockData blockData1 && blockData1.matches(blockData)) {
-					return true;
-				}
 			}
-		} else if (collisionEvent instanceof VehicleEntityCollisionEvent entityCollisionEvent && !blockSpecific) {
-			EntityData<?> entityData = EntityData.fromEntity(entityCollisionEvent.getEntity());
-			for (EntityData<?> entityData1 : (EntityData<?>[]) types) {
-				if (entityData1.isSupertypeOf(entityData))
+			for (BlockData blockData : blockDatas) {
+				if (blockData.matches(eventBlockData))
+					return true;
+			}
+		} else if (collisionEvent instanceof VehicleEntityCollisionEvent entityCollisionEvent) {
+			EntityData<?> eventEntityData = EntityData.fromEntity(entityCollisionEvent.getEntity());
+			for (EntityData<?> entityData : entityDatas) {
+				if (entityData.isSupertypeOf(eventEntityData))
 					return true;
 			}
 		}
