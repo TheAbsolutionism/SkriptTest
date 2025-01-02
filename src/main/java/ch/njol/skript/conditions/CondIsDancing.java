7f2f6642378ea9b613c14850a9751da159beed40
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2025-01-02 12:59:22 -0500

    Initial Commit

---
+++ src/main/java/ch/njol/skript/conditions/CondIsDancing.java
@@ -0,0 +1,40 @@
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.entity.Allay;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Parrot;
+
+@Name("Is Dancing")
+@Description("Checks to see if an entity is dancing. (e.g allays and parrots)")
+@Examples({
+	"if last spawned allay is dancing:",
+		"\tbroadcast \"Dance Party!\""
+})
+@Since("INSERT VERSION")
+public class CondIsDancing extends PropertyCondition<LivingEntity> {
+
+	static {
+		PropertyCondition.register(CondIsDancing.class, PropertyType.BE, "dancing", "livingentities");
+	}
+
+	@Override
+	public boolean check(LivingEntity entity) {
+		if (entity instanceof Allay allay) {
+			return allay.isDancing();
+		} else if (entity instanceof Parrot parrot) {
+			return parrot.isDancing();
+		}
+		return false;
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "dancing";
+	}
+
+}
---
+++ src/main/java/ch/njol/skript/effects/EffAllayDancing.java
@@ -0,0 +1,91 @@
+package ch.njol.skript.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.SyntaxStringBuilder;
+import ch.njol.skript.util.Direction;
+import ch.njol.util.Kleenean;
+import org.bukkit.Location;
+import org.bukkit.entity.Allay;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Make Allay Dance")
+@Description({
+	"Make an allay start or stop dancing.",
+	"By providing a location when making an allay dance, checks to see if the block at the location is a jukebox and playing music.",
+	"By not providing a location, the allay will non-stop dance."
+})
+@Examples({
+	"if last spawned allay is not dancing:",
+		"\tmake last spawned allay start dancing"
+})
+@Since("INSERT VERSION")
+public class EffAllayDancing extends Effect {
+
+	static {
+		Skript.registerEffect(EffAllayDancing.class,
+			"make %livingentities% (start dancing|dance) [from %-direction% %-location%]",
+			"make %livingentities% (stop dancing|not dance)");
+	}
+
+	private Expression<LivingEntity> entities;
+	private boolean start;
+	private @Nullable Expression<Location> location;
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		//noinspection unchecked
+		entities = (Expression<LivingEntity>) exprs[0];
+		start = matchedPattern == 0;
+		if (start && exprs[1] != null) {
+			//noinspection unchecked
+			Expression<Direction> direction = (Expression<Direction>) exprs[1];
+			//noinspection unchecked
+			Expression<Location> location = (Expression<Location>) exprs[2];
+			this.location = Direction.combine(direction, location);
+		}
+		return true;
+	}
+
+	@Override
+	protected void execute(Event event) {
+		Location location = this.location == null ? null : this.location.getSingle(event);
+		for (LivingEntity entity : entities.getArray(event)) {
+			if (!(entity instanceof Allay allay))
+				continue;
+			if (!start) {
+				allay.stopDancing();
+				continue;
+			}
+			if (location == null) {
+				allay.startDancing();
+				continue;
+			}
+			allay.startDancing(location);
+		}
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		SyntaxStringBuilder builder = new SyntaxStringBuilder(event, debug);
+		builder.append("make", entities);
+		if (start) {
+			builder.append("start");
+		} else {
+			builder.append("stop");
+		}
+		builder.append("dancing");
+		if (location != null)
+			builder.append("from", location);
+		return builder.toString();
+	}
+
+}
