19b50eb8122cdd09bf268e8414e21d49d8c1248a
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2025-01-30 00:36:07 -0500

    Initial Commit

--- src/main/java/ch/njol/skript/expressions/ExprRound.java
+++ src/main/java/ch/njol/skript/expressions/ExprRound.java
@@ -1,10 +1,6 @@
 package ch.njol.skript.expressions;
 
-import org.bukkit.event.Event;
-import org.jetbrains.annotations.Nullable;
-
 import ch.njol.skript.Skript;
-import org.skriptlang.skript.lang.converter.Converter;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
@@ -13,44 +9,59 @@ import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.SyntaxStringBuilder;
 import ch.njol.util.Kleenean;
 import ch.njol.util.Math2;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
 
-/**
- * @author Peter Güttinger
- */
 @Name("Rounding")
 @Description("Rounds numbers normally, up (ceiling) or down (floor) respectively.")
-@Examples({"set {var} to rounded health of player",
-		"set line 1 of the block to rounded \"%(1.5 * player's level)%\"",
-		"add rounded down argument to the player's health"})
+@Examples({
+	"set {var} to rounded health of player",
+	"set line 1 of the block to rounded \"%(1.5 * player's level)%\"",
+	"add rounded down argument to the player's health"
+})
 @Since("2.0")
 public class ExprRound extends PropertyExpression<Number, Long> {
+
 	static {
 		Skript.registerExpression(ExprRound.class, Long.class, ExpressionType.PROPERTY,
-				"(a|the|) round[ed] down %number%",
-				"(a|the|) round[ed] %number%",
-				"(a|the|) round[ed] up %number%");
+				"(a|the|) (round[ed] down|floored) %numbers%",
+				"%numbers% (round[ed] down|floored)",
+				"(a|the|) round[ed] %numbers%",
+				"%numbers% round[ed]",
+				"(a|the|) (round[ed] up|ceiled) %numbers%",
+				"%numbers% (round[ed] up|ceiled)"
+			);
 	}
 	
-	int action;
-	
-	@SuppressWarnings({"unchecked", "null"})
+	private int action;
+
 	@Override
 	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
+		//noinspection unchecked
 		setExpr((Expression<? extends Number>) exprs[0]);
-		action = matchedPattern - 1;
+		action = matchedPattern / 2;
 		return true;
 	}
 	
 	@Override
-	protected Long[] get(final Event e, final Number[] source) {
-		return get(source, n -> {
-			if (n instanceof Integer)
-				return n.longValue();
-			else if (n instanceof Long)
-				return (Long) n;
-			return action == -1 ? Math2.floor(n.doubleValue()) : action == 0 ? Math2.round(n.doubleValue()) : Math2.ceil(n.doubleValue());
+	protected Long @Nullable [] get(Event event, Number[] source) {
+		return get(source, number -> {
+			if (number instanceof Integer integer) {
+				return integer.longValue();
+			} else if  (number instanceof Long long1) {
+				return long1;
+			}
+
+			if (action == 0) {
+				return Math2.floor(number.doubleValue());
+			} else if (action == 1) {
+				return Math2.round(number.doubleValue());
+			} else {
+				return Math2.ceil(number.doubleValue());
+			}
 		});
 	}
 	
@@ -60,8 +71,17 @@ public class ExprRound extends PropertyExpression<Number, Long> {
 	}
 	
 	@Override
-	public String toString(final @Nullable Event e, final boolean debug) {
-		return (action == -1 ? "floor" : action == 0 ? "round" : "ceil") + "(" + getExpr().toString(e, debug) + ")";
+	public String toString(@Nullable Event event, boolean debug) {
+		SyntaxStringBuilder builder = new SyntaxStringBuilder(event, debug);
+		if (action == 0) {
+			builder.append("floor");
+		} else if (action == 1) {
+			builder.append("round");
+		} else {
+			builder.append("ceil");
+		}
+		builder.append(getExpr());
+		return builder.toString();
 	}
 	
 }
