2b4dd6e8852f90c12579adf6ac7021fbc2f926c4
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2025-01-31 03:15:34 -0500

    Initial Commit

---
+++ src/main/java/ch/njol/skript/expressions/ExprExcept.java
@@ -0,0 +1,81 @@
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.util.LiteralUtils;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Except")
+@Description("Filter a list by providing objects to be excluded.")
+@Examples({
+	"spawn zombie at location(0, 0, 0):",
+		"\thide entity from all players except {_player}",
+	"",
+	"set {_items::*} to a copper ingot, an iron ingot and a gold ingot",
+	"set {_except::*} to {_items::*} excluding copper ingot"
+})
+@Since("INSERT VERSION")
+public class ExprExcept extends SimpleExpression<Object> {
+
+	static {
+		Skript.registerExpression(ExprExcept.class, Object.class, ExpressionType.COMBINED,
+			"%objects% (except|excluding|not including) %objects%");
+	}
+
+	private Expression<?> source;
+	private Expression<?> exclude;
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		source = LiteralUtils.defendExpression(exprs[0]);
+		if (source.isSingle() || !LiteralUtils.canInitSafely(source))
+			return false;
+
+		exclude = LiteralUtils.defendExpression(exprs[1]);
+		if (!LiteralUtils.canInitSafely(exclude))
+			return false;
+
+		return true;
+	}
+
+	@Override
+	protected Object @Nullable [] get(Event event) {
+		Object[] exclude = this.exclude.getArray(event);
+		if (exclude == null || exclude.length == 0)
+			return source.getArray(event);
+
+		return source.stream(event)
+			.filter(sourceObject -> {
+				for (Object excludeObject : exclude)
+					if (sourceObject.equals(excludeObject))
+						return false;
+				return true;
+			})
+			.toArray();
+	}
+
+	@Override
+	public boolean isSingle() {
+		return false;
+	}
+
+	@Override
+	public Class<?> getReturnType() {
+		return Object.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return source.toString(event, debug) + " except " + exclude.toString(event, debug);
+	}
+
+}
