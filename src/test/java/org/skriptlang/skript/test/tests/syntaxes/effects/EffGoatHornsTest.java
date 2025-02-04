42053ce6d58474c1e7bcfe2cfdc1a1b531a618fe
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2025-02-04 04:35:51 -0500

    No Work

--- src/test/java/org/skriptlang/skript/test/tests/syntaxes/effects/EffGoatHornsTest.java
+++ src/test/java/org/skriptlang/skript/test/tests/syntaxes/effects/EffGoatHornsTest.java
@@ -3,17 +3,23 @@ package org.skriptlang.skript.test.tests.syntaxes.effects;
 import ch.njol.skript.test.runner.SkriptJUnitTest;
 import org.bukkit.Bukkit;
 import org.bukkit.event.world.WorldLoadEvent;
+import org.junit.After;
 import org.junit.Test;
 
 public class EffGoatHornsTest extends SkriptJUnitTest {
 
 	static {
-		setShutdownDelay(10);
+		setShutdownDelay(30);
 	}
 
 	@Test
 	public void test() {
 		Bukkit.getPluginManager().callEvent(new WorldLoadEvent(getTestWorld()));
+	}
+
+	@After
+	public void cleanup() {
+		// nothing
 	}
 
 }
